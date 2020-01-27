aa03d5888ce6bcca73a1db2cfe7b2f12f68c9556
Beth Skurrie <beth@bethesque.com>
2020-01-28 10:19:56 +1100

    chore: update release scripts to generate change logs on docker

--- script/next-docker-tag.sh
+++ script/next-docker-tag.sh
@@ -1,5 +1,7 @@
+set -e
 DOCKER_IMAGE="dius/pact-broker"
-gem_version=$(BUNDLE_GEMFILE=pact_broker/Gemfile bundle exec ruby -e "require 'pact_broker/version'; puts PactBroker::VERSION")
+# TODO pass this in
+gem_version=$(docker run --rm -v ${PWD}/pact_broker/Gemfile.lock:/tmp/Gemfile.lock ruby:2.6.4-alpine ruby -e "puts File.read('/tmp/Gemfile.lock').match(/pact_broker \((.*)\)/)[1]")
 existing_tags=$(wget -q https://registry.hub.docker.com/v1/repositories/${DOCKER_IMAGE}/tags -O - | jq -r .[].name)
 existing_release_numbers_for_current_gem_version=$(echo "$existing_tags" | grep "${gem_version}-" | sed 's/'${gem_version}'-//g')
 last_release_number=$(printf "0\n${existing_release_numbers_for_current_gem_version}" | sort -g | tail -1)
--- script/release.sh
+++ script/release.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 set -e
 
 export TAG=$(script/next-docker-tag.sh)
