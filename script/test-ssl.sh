bf5704eb22188ef46a8017f40f607c7fe0849fe4
Beth Skurrie <beth@bethesque.com>
2020-01-28 21:55:04 +1100

    spike: update test-ssl script - it won't work thought

--- script/test-ssl.sh
+++ script/test-ssl.sh
@@ -2,10 +2,15 @@
 export SSL_CERT_FILE=ssl/cacert.crt
 curl -v https://localhost:8443
 
+# THIS IS NOT GOING TO WORK because it needs a proper host name,
+# not localhost - this will try and connect to the localhost of the
+# pact-cli docker container
+
 docker run --rm \
- -e SSL_CERT_FILE=ssl/cacert.crt \
+ -e SSL_CERT_FILE=/tmp/cacert.crt \
+ -v ${PWD}/ssl/cacert.crt:/tmp/cacert.crt \
   pactfoundation/pact-cli:latest \
   broker can-i-deploy \
   --pacticipant Foo \
   --latest \
-  --broker-base-url https://localhost:8443
+  --broker-base-url https://pact_broker:8443
