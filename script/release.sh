d5bf73eb27898e205c2850d3c41a8d9a349b6f53
Beth Skurrie <beth@bethesque.com>
2020-01-29 08:05:31 +1100

    chore: update scripts
    [ci-skip]

--- script/release.sh
+++ script/release.sh
@@ -3,6 +3,7 @@
 set -e
 
 export TAG=$(script/release/next-docker-tag.sh)
+echo "Releasing tag ${TAG}"
 script/release/generate-changelog.sh
 git add CHANGELOG.md && git commit -m "chore(changelog): update for ${TAG}"
 echo "Tagging version ${TAG}"
