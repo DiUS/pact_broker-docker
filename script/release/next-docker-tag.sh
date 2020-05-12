2ef434a618db0e98cfa0b0f3146384a22f6349fd
Beth Skurrie <beth@bethesque.com>
2020-05-12 12:35:12 +1000

    feat: change tagging format from M.m.p-RELEASE to M.m.p.RELEASE to support dependabot

--- script/release/next-docker-tag.sh
+++ script/release/next-docker-tag.sh
@@ -6,5 +6,5 @@ existing_tags=$(wget -q https://registry.hub.docker.com/v1/repositories/${DOCKER
 existing_release_numbers_for_current_gem_version=$(echo "$existing_tags" | grep "${gem_version}-" | sed 's/'${gem_version}'-//g')
 last_release_number=$(printf "0\n${existing_release_numbers_for_current_gem_version}" | sort -g | tail -1)
 next_release_number=$[$last_release_number+1]
-tag="${gem_version}-${next_release_number}"
+tag="${gem_version}.${next_release_number}"
 echo $tag
