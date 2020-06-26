4d5db5a58e42a941336dd88bd2e999df4e5cd13d
Beth Skurrie <beth@bethesque.com>
2020-06-26 10:28:52 +1000

    chore: update copy_from_pf_image.sh

--- script/copy_from_pf_image.sh
+++ script/copy_from_pf_image.sh
@@ -6,6 +6,5 @@ cp ../pact-broker-docker/pact_broker/database_connection.rb pact_broker/database
 cp ../pact-broker-docker/pact_broker/pact_broker_resource_access_policy.rb pact_broker/pact_broker_resource_access_policy.rb
 cp ../pact-broker-docker/pact_broker/basic_auth.rb pact_broker/basic_auth.rb
 cp ../pact-broker-docker/pact_broker/resource_access_rules.rb pact_broker/resource_access_rules.rb
-cp ../pact-broker-docker/pact_broker/database_logger.rb pact_broker/database_logger.rb
 cp ../pact-broker-docker/pact_broker/logger.rb pact_broker/logger.rb
 cp ../pact-broker-docker/pact_broker/docker_configuration.rb pact_broker/docker_configuration.rb
