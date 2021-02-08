0e685d1a096b427375e5fda31f56f1594cde33cf
Matt Fellows <matt.fellows@onegeek.com.au>
2021-02-08 18:16:58 +1100

    feat: run trivy security scanner during release

---
+++ script/scan-inside-docker-container.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+set -eu
+
+apt-get update -y && apt-get install -y wget
+wget -q -O - https://raw.githubusercontent.com/aquasecurity/trivy/master/contrib/install.sh | sh -s -- -b /usr/local/bin
+trivy filesystem --exit-code 1 --quiet /
