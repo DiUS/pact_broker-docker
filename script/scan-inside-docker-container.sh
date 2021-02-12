c85ebd01adc72e10e56a761d88d6405968bc5bcd
Beth Skurrie <beth@bethesque.com>
2021-02-12 11:05:54 +1100

    chore: use --no-progress instead of --quiet as --quiet is not supported on this version :shrug:

--- script/scan-inside-docker-container.sh
+++ script/scan-inside-docker-container.sh
@@ -3,4 +3,4 @@ set -eu
 
 apt-get update -y && apt-get install -y wget
 wget -q -O - https://raw.githubusercontent.com/aquasecurity/trivy/master/contrib/install.sh | sh -s -- -b /usr/local/bin
-trivy filesystem --exit-code 1 --quiet /
+trivy filesystem --exit-code 1 --no-progress /
