cf17a8f40652ae8dc9337f9dad618cd2d8b01f78
Beth Skurrie <beth@bethesque.com>
2021-02-15 09:43:06 +1100

    chore: update scan workflow

--- script/scan-workflow/run.sh
+++ script/scan-workflow/run.sh
@@ -5,7 +5,7 @@ set -euo >/dev/null
 script_dir=$(cd "$(dirname $0)" && pwd)
 
 if [ "${GITHUB_ACTIONS:-}" = "true" ]; then
-  ${script_dir}/docker-login.sh
+  ${script_dir}/../release-workflow/docker-login.sh
 fi
 
 export DOCKER_IMAGE_ORG_AND_NAME=dius/pact-broker
