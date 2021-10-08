226b15669e8a42c0fb332a51af3cf8cb2c58236f
Beth Skurrie <beth@bethesque.com>
2021-10-08 13:57:21 +1100

    chore: update build workflow

--- script/build-workflow/run.sh
+++ script/build-workflow/run.sh
@@ -8,5 +8,7 @@ if [ "${GITHUB_ACTIONS:-}" = "true" ]; then
   echo ${DOCKER_HUB_TOKEN} | docker login --username ${DOCKER_HUB_USERNAME} --password-stdin
 fi
 
-${script_dir}/docker-build.sh
+. ${script_dir}/../release-workflow/set-env-vars.sh
+
+${script_dir}/../release-workflow/docker-build.sh
 ${script_dir}/../scan.sh ${DOCKER_IMAGE_ORG_AND_NAME}:latest
